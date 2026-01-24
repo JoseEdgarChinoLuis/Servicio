01bb7550a3584f92084e6a20625f1e8780ce49f8
JoseEdgarChinoLuis <20253rd009@utez.edu.mx>
2026-01-23 21:40:14 -0600

    Segundo commit

--- Servicio.java
+++ Servicio.java
@@ -10,3 +10,15 @@ public class Servicio {
         System.out.println("Ingresa tu genero (H/M):");
         genero = scanner.next().charAt(0);
 
+        System.out.println("Ingresa tu edad:");
+        edad = scanner.nextInt();
+
+        if (edad >= 18 && (genero == 'H' || genero == 'h')) {
+            System.out.println("Te toca hacer servicio militar");
+        } else {
+            System.out.println("No haces servicio militar");
+        }
+
+        scanner.close();
+    }
+}
