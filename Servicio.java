70bf531c02b2ec9c15161f2987c18029bb3fee7e
JoseEdgarChinoLuis <20253rd009@utez.edu.mx>
2026-01-23 21:39:24 -0600

    Primer commit

---
+++ Servicio.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Servicio {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        char genero;
+        int edad;
+
+        System.out.println("Ingresa tu genero (H/M):");
+        genero = scanner.next().charAt(0);
+
