0a3ed86859a86d5a33c20ee3e5fce3ebc6dd4ca8
JoseEdgarChinoLuis <20253rd009@utez.edu.mx>
2026-01-28 22:09:01 -0600

    primer commit

---
+++ Contrasena.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Contrasena {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean bandera = false;
+
+        System.out.println("Ingresa el usuario:");
+        String usuarioIngresado = sc.nextLine();
+
+        System.out.println("Ingresa la contraseña:");
+        String passwordIngresado = sc.nextLine();
+
+        
